8107f5d87f9bd716c56b61c18bc771d679be0add
jongwhan <jonghwan@crypto.com>
2023-12-20 22:45:38 +0900

    simple

---
+++ go.sh
@@ -0,0 +1,15 @@
+# Check if ./llama.cpp/models/7B exists
+if [ ! -d "./llama.cpp/models/7B" ]; then
+    cd llama
+    ./download2.sh
+
+    cp ./llama/tokenizer.model ./llama.cpp/models
+    cp ./llama/tokenizer_checklist.chk ./llama.cpp/models
+    cp -Rf ./llama/llama-2-7b-chat ./llama.cpp/models/7B
+fi
+
+# Continue with the rest of the script
+cd llama.cpp
+./build.sh
+./go.sh
+cd ..
